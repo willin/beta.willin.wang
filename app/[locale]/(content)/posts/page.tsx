f9492ec9e3ec1a53e83e9c0106bb8a45252f17e2
Willin Wang <willin@willin.org>
2022-11-29 01:34:45 +0800

    test: headers

--- app/[locale]/(content)/posts/page.tsx
+++ app/[locale]/(content)/posts/page.tsx
@@ -1,7 +1,20 @@
+import { headers } from 'next/headers';
+
 export default async function PostPage1() {
+  const headersList = headers();
+  const referer = headersList.get('referer');
+  const ua = headersList.get('user-agent');
+  const country = headersList.get('x-vercel-ip-country');
+  const region = headersList.get('x-vercel-ip-country-region');
+
   return (
     <div>
       <h1>Hello</h1>
+      <div>Referer: {referer}</div>
+      <div>UA: {ua}</div>
+      <div>Country: {country}</div>
+      <div>Region: {region}</div>
+      <pre>{JSON.stringify(headersList.entries(), null, 2)}</pre>
     </div>
   );
 }
