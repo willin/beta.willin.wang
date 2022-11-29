fd29f25f8835b2385c71b3f7f290b7f2cfa8088b
Willin Wang <willin@willin.org>
2022-11-29 18:29:39 +0800

    test: edge runtime

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
   experimental: {
-    // runtime: 'experimental-edge', // 'node.js' (default) | 'experimental-edge'
+    runtime: 'experimental-edge', // 'node.js' (default) | 'experimental-edge'
     appDir: true
   }
 };
