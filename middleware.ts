028e70bd5ebfed0ab8b0a3c29010df3f317cbefb
Willin Wang <willin@willin.org>
2022-11-29 11:56:29 +0800

    feat: add robots

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,4 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { NextResponse, type NextRequest } from 'next/server';
 import { supportedLanguages, fallbackLng } from './i18n';
 
 export function middleware(request: NextRequest) {
@@ -8,7 +7,7 @@ export function middleware(request: NextRequest) {
   const { headers, nextUrl } = request;
 
   // Exclude statics - add your static folders
-  const shouldCheckLocale = !nextUrl.pathname.startsWith('/_next') && !nextUrl.pathname.startsWith('/images') && !nextUrl.pathname.endsWith('.cur');
+  const shouldCheckLocale = !nextUrl.pathname.startsWith('/_next') && !nextUrl.pathname.endsWith('.cur') && !nextUrl.pathname.endsWith('.txt');
 
   const reqLocale = nextUrl.pathname.split('/')[1];
   const noValidLocale = !locales.includes(reqLocale);
