bf6ea1fea8f92d0f0eb34a6876a5f9f425e87284
Willin Wang <willin@willin.org>
2022-12-06 01:44:38 +0800

    fix: locale-link

--- ui/locale-link.tsx
+++ ui/locale-link.tsx
@@ -3,7 +3,7 @@ import { fallbackLng } from '@/i18n';
 import Link, { LinkProps } from 'next/link';
 import { useI18n } from '../i18n/hook';
 
-export function LocaleLink({ href, children, ...props }: LinkProps & { href: string }) {
+export function LocaleLink({ href, children, ...props }: LinkProps & { href: string; children: React.ReactNode }) {
   const { locale } = useI18n();
   const localeHref = locale() === fallbackLng ? href : `/${locale()}${href}`;
 
