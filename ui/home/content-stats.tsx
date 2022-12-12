7ba3c7c890e0f9ed4453913d6af49fa8321c10fd
Willin Wang <willin@willin.org>
2022-12-13 00:58:45 +0800

    fix: locale

--- ui/home/content-stats.tsx
+++ ui/home/content-stats.tsx
@@ -21,19 +21,19 @@ type InteractionStatsProps = {
 
 export function SiteStates({ stats, interactions }: { stats: ContentStatsProps[]; interactions: InteractionStatsProps[] }) {
   const { t, locale } = useI18n();
+  const lang = locale() as 'zh-CN';
+
   const totalContents = stats.reduce<number>((acc, item) => acc + item.count, 0);
-  const localeContents = stats.filter((item) => item.locale === locale).reduce<number>((acc, item) => acc + item.count, 0);
+  const localeContents = stats.filter((item) => item.locale === lang).reduce<number>((acc, item) => acc + item.count, 0);
   const totalWordcount = stats.reduce<number>((acc, item) => acc + item.sum.wordcount, 0);
-  const localeWordcount = stats.filter((item) => item.locale === locale).reduce<number>((acc, item) => acc + item.sum.wordcount, 0);
+  const localeWordcount = stats.filter((item) => item.locale === lang).reduce<number>((acc, item) => acc + item.sum.wordcount, 0);
   const totalReadtime = Math.ceil(stats.reduce<number>((acc, item) => acc + item.sum.readtime, 0) / 60);
-  const localeReadtime = Math.ceil(stats.filter((item) => item.locale === locale).reduce<number>((acc, item) => acc + item.sum.readtime, 0) / 60);
+  const localeReadtime = Math.ceil(stats.filter((item) => item.locale === lang).reduce<number>((acc, item) => acc + item.sum.readtime, 0) / 60);
 
   const totalViews = interactions.reduce<number>((acc, item) => acc + item.sum.views, 0);
-  const localeViews = interactions.filter((item) => item.locale === locale).reduce<number>((acc, item) => acc + item.sum.views, 0);
+  const localeViews = interactions.filter((item) => item.locale === lang).reduce<number>((acc, item) => acc + item.sum.views, 0);
   // const totalLikes = interactions.reduce<number>((acc, item) => acc + item.sum.likes, 0);
-  // const localeLikes = interactions.filter((item) => item.locale === locale).reduce<number>((acc, item) => acc + item.sum.likes, 0);
-
-  const lang = locale() as 'zh-CN';
+  // const localeLikes = interactions.filter((item) => item.locale === lang).reduce<number>((acc, item) => acc + item.sum.likes, 0);
 
   return (
     <div className='stats stats-vertical lg:stats-horizontal shadow my-2 flex'>
