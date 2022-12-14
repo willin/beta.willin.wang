12a0cd0f9ddf3a97c42c78d0b094cf9dac42f401
Willin Wang <willin@willin.org>
2022-12-15 01:49:51 +0800

    fix: edge

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,10 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-export const config = {
-  runtime: 'experimental-edge'
-};
-
 type Data = {
   name: string;
 };
