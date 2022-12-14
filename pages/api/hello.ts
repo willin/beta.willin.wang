50c89a552866d71aa9dba9b9841ea5876c31e41c
Willin Wang <willin@willin.org>
2022-12-15 01:39:49 +0800

    experimental-edge

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
 
+export const config = {
+  runtime: 'experimental-edge'
+};
+
 type Data = {
   name: string;
 };
